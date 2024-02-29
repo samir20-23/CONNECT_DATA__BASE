a01ad1699b1805d452ca8605f2258a724fa855bc
kali_code <144660881+samir20-23@users.noreply.github.com>
2024-02-29 13:01:47 +0100

    Update 4INSERT-DATA.php

--- 4INSERT-DATA.php
+++ 4INSERT-DATA.php
@@ -1,39 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    // if($_SERVER['REQUEST_METHOD']=="POST"){
-    // try{
-    //     $con = new PDO("mysql:host=localhost;dbname=samir","SAMIR","samir123");
-    //     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //     $insert = "INSERT INTO LOGIN(email,password)
-    //     VALUES('smair@gmail.com','samir1234567')";
-    //     $con->exec($insert);
-
-    // }   
-    // catch(PDOException $e){echo "note connect" . $e->getMessage();}
-    // }
-    ?>
-    <form method="post" enctype="multipart/form-data" action="<?php //echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-     <input type="text" name="ex1">
-     <input type="submit">
-   </form>
-</body>
-</html>
- -->
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-            body{ margin:20px;display: flex; justify-content: center; zoom:1.7;}
-    p{color:green; text-shadow: 0 0 12px green;position: absolute; top: -11px; left: 45%;}
-    pre{color:red; text-shadow:  0 0 12px red ;position: absolute; top: -11px; left: 43%;}
-@keyframes pulse {0% {transform: scale(1);}100% {transform: scale(1.1);}}
+        body{display: flex; justify-content: center; zoom: 1.5;}
+        input{margin: 10px;}
+        #dbname{ border: 3px solid black; width: 180px; margin-left: -1px;}
+        #table{ border: 2px solid black; width: 180px; margin-left: -1px;}
+        p{color: green; text-shadow:  0 0 12px green; position: absolute;}
+        span{color: red; text-shadow:  0 0 12px red; position: absolute;}
+        pre{margin-top: 40px;}
+        @keyframes pulse {0% {transform: scale(1);}100% {transform: scale(1.1);}}
 #submit{animation: pulse 1s alternate-reverse infinite;
         box-shadow: 5px 5px 10px 1px rgba(0, 0, 0, 0.3);
         padding: 7px 20px;
@@ -52,32 +20,57 @@
         color: #fff;
         border-radius: 5px; 
         position: relative;
-        left: 23%;
+        top: 87%;
+        left: 40%;
 }
     </style>
 </head>
 <body>
-<?php
-if($_SERVER['REQUEST_METHOD']=="POST"){
-    $values = $_POST['values'];
-   try{
-    $con =new PDO("mysql:host=localhost;dbname=test","SAMIR","samir123");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <?php
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        //fiilter data
+        function funcdata($data){
+            $data=htmlspecialchars($data);
+            $data=stripslashes($data);
+            $data=trim($data);
+            return $data;
+        }
+        //posts
+    $table     = funcdata($_POST['table']);
 
-    $valuesdata ="INSERT INTO $values(firstname , lastname , email)
-    VALUES ('morad','aoulad','morad@gmail.com')";
-    $con->exec($valuesdata);
+    $Column1   = funcdata($_POST['Column1']);
+    $Column2   = funcdata($_POST['Column2']);
+    $Column3   = funcdata($_POST['Column3']);
+    //connect
+try{
+    $con = new PDO("mysql:host=localhost;dbname=mydata","SAMIR","samir123");
+    $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
 
-    echo "<p>* DONE *</p>";
-   }
-   catch(PDOException){echo "<pre>* NOTE CONNECT *</pre>";}
+
+    $sql = "CREATE TABLE $table (
+        $Column1 VARCHAR(30) NOT NULL,
+        $Column2 VARCHAR(50)NOT NULL,
+        $Column3 VARCHAR(50)
+    )";
+$con->exec($sql);
+    echo "<p>* CONNECT *</p>";
+    header("location:4insert-data.php");
 }
-?>
+catch(PDOException  ){echo "<span>* NOTE CONNECT *</span>";}
 
-    <form method="post" enctype="multipart/form-data"  action="<?php echo  htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-    <span> INSERT  THE DATABASE :</span><hr>
-    <input type="text" name="values"><hr>
-    <input id="submit" type="submit">
+
+    }
+    ?>
+    <form  method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        <pre>
+NAME TABL:<input  id="table" type="text" name="table">
+
+Column 1 :<input type="text" name="Column1" />
+Column 2 :<input type="text" name="Column2" />
+Column 3 :<input type="text" name="Column3" />
+
+<input id="submit" type="submit" name="submit" />
+</pre>
     </form>
 </body>
 </html>
