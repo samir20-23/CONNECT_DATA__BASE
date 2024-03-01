a3cfcd2a8e9ee6f71f1adc0a3d492fc32c7579ae
kali_code <144660881+samir20-23@users.noreply.github.com>
2024-03-01 16:00:00 +0100

    Update 1CONNECT.php

--- 1CONNECT.php
+++ 1CONNECT.php
@@ -7,7 +7,7 @@
 </head>
 <style>
     body{display: flex; justify-content: center; zoom:1.7;}
-    p{color:green; text-shadow: 0 0 12px green;}
+    p{color:green; text-shadow: 0 0 12px green; position: absolute;left: 44%;}
     pre{color:red; text-shadow:  0 0 12px red ;}
 @keyframes pulse {0% {transform: scale(1);}100% {transform: scale(1.1);}}
 #submit{animation: pulse 1s alternate-reverse infinite;
@@ -17,29 +17,34 @@
         color: #fff;
         border-radius: 5px; 
         position: relative;
-        top: 87%;
-        left: -94%;
+        top: 197%;
+        left: 9%;
 }
+#next{position: absolute; top: 78px;  left: 58%;}
 
 </style>
 <body>
+
     <?php
+    if(isset($_POST['next'])){ header("location:2creat-DB.php");};
     if($_SERVER['REQUEST_METHOD']=="POST"){
 
         try{
             $con=new PDO("mysql:host=localhost","SAMIR","samir123");
             $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-
-            echo "<p>CONNECT</p>";
+            echo "<p>CONNECT✅</p>";
+            
         }
         catch(PDOException){
-         echo "<pre>note connect</pre>";
+         echo "<pre>note connect❌</pre>";
+        
         }
     }
     ?>
 
     <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        <input id="submit" type="submit" value="CLICK">
+        <input id="submit" name="submit" type="submit" value="CLICK">
+        <button name="next" id="next">➡️</button>
     </form>
 
     <div>
