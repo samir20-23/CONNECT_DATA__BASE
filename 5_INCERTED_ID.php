c2154c884770bf623a871968ffcb485e3fb12cce
kali_code <144660881+samir20-23@users.noreply.github.com>
2024-03-01 16:22:35 +0100

    Update 5_INCERTED_ID.php

--- 5_INCERTED_ID.php
+++ 5_INCERTED_ID.php
@@ -6,10 +6,9 @@
     <title>Document</title>
     <style>
         body{display: flex; justify-content: center; zoom: 1.5;}
-        input{margin: 10px;}
-        #in1{margin: 0; border: 3px solid #FF7B7B;}
-        p{color: green; text-shadow:  0 0 12px green; position: absolute;}
-        #insert-id{position: absolute;top: -10px;}
+        input{margin: 10px;position: relative;left: 5%;}
+        p{color: green; text-shadow:  0 0 12px green; position: absolute;left: 41.5%;}
+        #insert-id{position: absolute;top: -10px;left: 38%;}
         span{color: red; text-shadow:  0 0 12px red; position: absolute;}
         pre{margin-top: 40px;}
         @keyframes pulse {0% {transform: scale(1);}100% {transform: scale(1.1);}}
@@ -21,7 +20,7 @@
         border-radius: 5px; 
         position: relative;
         top: 87%;
-        left: 1%;
+        left: -36%;
 }
     </style>
 </head>
@@ -37,15 +36,10 @@
         $c1 = filterdata($_POST['c1']);
         $c2 = filterdata($_POST['c2']);
         $c3 = filterdata($_POST['c3']);
-        $c4 = filterdata($_POST['c4']);
-        $c5 = filterdata($_POST['c5']);
-        $c6 = filterdata($_POST['c6']);
         try{
             $con = new PDO("mysql:host=localhost;dbname=test","SAMIR","samir123");
             $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
             $insert = "INSERT INTO data1(lastname,firstname,email) VALUES('$c1','$c2','$c3')";
-            $con->exec($insert);
-            $insert ="INSERT INTO data1(lastname,firstname,email) VALUES('$c4','$c5','$c6')";
             $con->exec($insert);
             $last_id= $con->lastInsertId();
             echo "<p>* IS CONNECT *</p>";
@@ -58,9 +52,9 @@
     ?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
 <pre>
-<input placeholder="first name value" type="text" name="c1">  <input placeholder="first name value" type="text" name="c4">
-<input placeholder="last  name value" type="text" name="c2">  <input placeholder="last  name value" type="text" name="c5">
-<input placeholder="email value" type="text" name="c3">  <input placeholder="email value" type="text" name="c6">
+<input placeholder="first name value" type="text" name="c1"> 
+<input placeholder="last  name value" type="text" name="c2"> 
+<input placeholder="email value" type="text" name="c3">
                     <input id="submit" type="submit" name="submit">
 </pre>
 </form>
